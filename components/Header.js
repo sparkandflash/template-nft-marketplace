c76ced6a8af7bc4f746e843c62c74a2c1bca96ae
plaaz <47806016+sparkandflash@users.noreply.github.com>
2023-01-16 01:05:00 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,10 +2,11 @@
 import {
     FormControl, Flex, Spacer,ButtonGroup, Heading, Input, FormLabel, Text, Button, Box, Stack, VStack
 } from "@chakra-ui/react"
-
-
+import { useEffect, useState } from "react";
+//import ServiceSearch from '../components/ServiceSearch';
 import { useRouter } from 'next/router'
-
+//make a function to check whether wallet connected, if yes -> show the disconnect button else dont show
+//<Button size= "lg">disconnect</Button>
 function Header() {
 
     const router = useRouter()
@@ -16,7 +17,7 @@ function Header() {
         <header>
             <Flex margin="6px"  minWidth='max-content' alignItems='center' gap='2' p={2}>
             <Box p='2'>
-    <Heading size='md'>sample-marketplace</Heading>
+    <Heading size='md'>generic nft marketplace</Heading>
   </Box>
   <Spacer />
   <ButtonGroup gap='2'>
